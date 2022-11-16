6062c04222e383c5025ac56bc391c49b7b123931
Jingru Xiang <xiang.jing@northeastern.edu>
2022-11-16 02:07:27 -0500

    ass8 modify

--- src/main/java/neu/edu/csye6225/config/SNSConfig.java
+++ src/main/java/neu/edu/csye6225/config/SNSConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,23 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class SNSConfig {
-    @Value("${aws.access_key_id}")
-    private String accessKeyId;
-    @Value("${aws.secret_access_key}")
-    private String secretAccessKey;
     @Value("${aws.s3.region}")
     private String region;
 
-
-
     @Bean
     public AmazonSNS getAmazonSNS() {
-        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(
-                new BasicAWSCredentials(accessKeyId, secretAccessKey)
-        );
-
         return AmazonSNSClientBuilder.standard()
-                .withCredentials(awsCredentialsProvider)
                 .withRegion(region)
                 .build();
     }
