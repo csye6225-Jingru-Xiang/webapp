e16b6ced726ef855faed34280a670b8cea242a41
Jingru Xiang <xiang.jing@northeastern.edu>
2022-11-17 15:38:35 -0500

    ass8 final

--- src/main/java/neu/edu/csye6225/config/DynamoConfig.java
+++ src/main/java/neu/edu/csye6225/config/DynamoConfig.java
@@ -1,27 +1,19 @@
 package neu.edu.csye6225.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoConfig {
-    @Value("${aws.s3.region}")
-    private String region;
 
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(AmazonDynamoDBClientBuilder.standard()
-                .withRegion(region)
-                .build());
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
     }
+
 }
--- src/main/java/neu/edu/csye6225/config/SNSConfig.java
+++ src/main/java/neu/edu/csye6225/config/SNSConfig.java
@@ -1,11 +1,7 @@
 package neu.edu.csye6225.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-import com.amazonaws.services.sns.model.PublishResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
